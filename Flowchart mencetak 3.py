2bb7724b35ecc4c3cdffdbcd35854c6b3fc6947b
Dika251204 <rizkidika449@gmail.com>
2024-09-20 15:56:54 +0800

    Create Flowchart mencetak 3.py

---
+++ Flowchart mencetak 3.py
@@ -0,0 +1,9 @@
+https://drive.google.com/file/d/1ZrTfBxXYCDiJh6rryNS8ecQrxh7wGBCI/view?usp=drivesdk
+pseudocode
+print("     *")
+print("    *.*")     
+print("   *.*.*")     
+print("  *.*.*.*")
+print(" * *.*.*.*")
+print("*.*.*.*.*.*")
+print("",end="")
